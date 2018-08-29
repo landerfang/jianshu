0ce276ad2f65a3b1b0e6150a6f85bb1ccb1a07bd
fangliude <fang.liude@intellif.com>
2018-08-29 10:53:24 +0800

    jianshu init

---
+++ src/pages/detail/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './detail.css';
+
+class Detail extends Component {
+  render() {
+    return (
+      <div className='detail'>detail
+        <a href="#" className="link">连接</a>
+      </div>
+    )
+  }
+}
+
+export default Detail;
---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='home-wrapper'>home
+        <a href="#" className="link">超链接</a>
+      </div>
+    )
+  }
+}
+
+export default Home;
